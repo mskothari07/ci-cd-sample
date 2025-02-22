719f381118d721ad109f4cb68fa413b30edb22b0
Meet Kothari <meet.kothari@blazeclan.com>
2025-02-23 00:22:19 +0530

    corrected the username

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World! My First CICD\n');
+  res.end('Hello, World! My First CI and CDDD\n');
 });
 
 server.listen(port, hostname, () => {
